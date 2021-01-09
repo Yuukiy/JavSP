50f18cb4c91b1462dc49007de3bc171e6d0849f4
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-01-09 22:52:09 +0800

    使用类来组织解析到的数据

---
+++ core/datatype.py
@@ -0,0 +1,20 @@
+"""定义数据类型"""
+
+
+class Movie:
+    def __init__(self, dvdid, cid=None, pid=None):
+        self.dvdid = dvdid          # DVD ID，即通常的番号
+        self.cid = cid              # DMM Content ID
+        self.pid = pid              # DMM Product ID
+        self.cover = None           # 封面图片
+        self.genre = None           # 影片分类的标签
+        self.title = None           # 影片标题（不含番号）
+        self.magnet = None          # 磁力链接
+        self.actress = None         # 出演女优
+        self.director = None        # 导演
+        self.duration = None        # 影片时长
+        self.producer = None        # 制作商
+        self.publisher = None       # 发行商
+        self.sample_pics = None     # 预览图片
+        self.publish_date = None    # 发布日期
+
--- web/javbus.py
+++ web/javbus.py
@@ -4,35 +4,47 @@ from datetime import date
 
 sys.path.append('../') 
 from web.base import get_html
+from core.datatype import Movie
 
 
 base_url = 'https://www.busfan.club'
 permanent_url = 'https://www.javbus.com'
 
 
-def parse_data(avid):
+def parse_data(movie: Movie):
     """解析指定番号的影片数据"""
-    html = get_html(f'{base_url}/{avid}')
+    html = get_html(f'{base_url}/{movie.dvdid}')
     container = html.xpath("/html/body/div[@class='container']")[0]
     title = container.xpath("h3/text()")[0]
     cover = container.xpath("//a[@class='bigImage']/img/@src")
-    avatar = container.xpath("//div[@class='star-name']/../a/img/@src")
+    # avatar = container.xpath("//div[@class='star-name']/../a/img/@src")
     sample_pics = container.xpath("div[@id='sample-waterfall']/a/div/img/@src")
     info = container.xpath("//div[@class='col-md-3 info']")[0]
     dvdid = info.xpath("p/span[text()='識別碼:']")[0].getnext().text
     date_str = info.xpath("p/span[text()='發行日期:']")[0].tail.strip()
-    pub_date = date.fromisoformat(date_str)
     duration = info.xpath("p/span[text()='長度:']")[0].tail.replace('分鐘', '').strip()
     director = info.xpath("p/span[text()='導演:']")[0].getnext().text.strip()
-    # 制作商
     producer = info.xpath("p/span[text()='製作商:']")[0].getnext().text.strip()
-    # 发行商
     publisher = info.xpath("p/span[text()='發行商:']")[0].getnext().text.strip()
     genre_tags = info.xpath("p[text()='類別:']")[0].getnext().xpath("span/a")
+    actress = info.xpath("//div[@class='star-name']/a/@title")
+    magnet = html.xpath("//table[@id='magnet-table']/tr/td[1]/a/@href")
     genre = [i.text for i in genre_tags]
-    magnets = html.xpath("//table[@id='magnet-table']/tr/td[1]/a/@href")
-    return
+    # 整理数据并更新movie的相应属性
+    movie.title = title.replace(dvdid, '').strip()
+    movie.cover = cover
+    movie.sample_pics = sample_pics
+    movie.publish_date = date.fromisoformat(date_str)
+    movie.duration = duration
+    movie.director = director
+    movie.producer = producer
+    movie.publisher = publisher
+    movie.genre = genre
+    movie.actress = actress
+    movie.magnet = magnet
 
 
 if __name__ == "__main__":
-    parse_data('ipx-177')
+    movie = Movie('ipx-177')
+    parse_data(movie)
+    pass
