d6a5340f7e072c0fa3609308b0b1ec7c3696ed9d
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-01-09 15:24:23 +0800

    解析avsox, javdb, javlib的防屏蔽地址

--- web/proxyfree.py
+++ web/proxyfree.py
@@ -10,10 +10,14 @@ from web.base import get_html, is_connectable
 def get_proxy_free_url(site_name: str) -> str:
     """获取指定网站的免代理地址"""
     site_name = site_name.lower()
-    if site_name == 'javbus':
+    if site_name == 'avsox':
+        return _choose_one(_get_avsox_urls())
+    elif site_name == 'javbus':
         return _choose_one(_get_javbus_urls())
     elif site_name == 'javlib':
-        return ''
+        return _choose_one(_get_javlib_urls())
+    elif site_name == 'javdb':
+        return _choose_one(_get_javdb_urls())
     else:
         raise Exception("Dont't know how to get proxy-free url for " + site_name)
 
@@ -26,6 +30,14 @@ def _choose_one(urls) -> str:
     return urls[-1]
 
 
+def _get_avsox_urls() -> list:
+    resp = get_html('https://tellme.pw/avsox')
+    html = lxml.html.fromstring(resp)
+    urls = html.xpath('//h4/strong/a/@href')
+    # 得到的urls的最后一个地址通常就是avsox的永久地址
+    return urls
+
+
 def _get_javbus_urls() -> list:
     resp = get_html('https://www.javbus.one/')
     html = lxml.html.fromstring(resp)
@@ -34,7 +46,25 @@ def _get_javbus_urls() -> list:
     # 始终将永久地址附加到最后
     urls.append('https://www.javbus.com/')
     return urls
-    
+
+
+def _get_javlib_urls() -> list:
+    resp = get_html('https://www.ebay.com/usr/javlibrary')
+    html = lxml.html.fromstring(resp)
+    text = html.xpath('//h2[@class="bio inline_value"]')[0].text_content()
+    match = re.search(r'[\w\.]+', text, re.A)
+    if match:
+        domain = f'https://www.{match.group(0)}.com'
+        return [domain]
+
+
+def _get_javdb_urls() -> list:
+    resp = get_html('https://lynnconway.me/javdbnews')
+    html = lxml.html.fromstring(resp)
+    text = html.xpath('//p[@class="text3"]')[0].text_content()
+    urls = [i.strip() for i in text.split('/')]
+    return urls
+
 
 if __name__ == "__main__":
-    print(get_proxy_free_url('javbus'))
+    print(get_proxy_free_url('javlib'))
