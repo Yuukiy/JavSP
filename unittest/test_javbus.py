af6dfff58507e1d44741cc570ab7770235637056
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-01-10 19:07:12 +0800

    添加javbus.py的测试用例

---
+++ unittest/test_javbus.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+sys.path.append('../')
+from web.javbus import *
+
+
+data_dir = os.path.split(__file__)[0] + os.sep + 'data'
+
+
+def compare(dvdid):
+    """从本地的数据文件生成Movie实例，并与在线抓取到的数据进行比较"""
+    path = data_dir + os.sep + f'{dvdid} (javbus).json'
+    local = Movie(from_file=path)
+    online = Movie(dvdid)
+    parse_data(online)
+    # 解包数据再进行比较，以便测试不通过时快速定位不相等的键值
+    local_vars = vars(local)
+    online_vars = vars(online)
+    for k, v in online_vars.items():
+        assert v == local_vars.get(k, None)
+
+
+def test_case1():
+    compare('IPX-177')
