8fd458208992e7f30d1ec759c14cb8db88c090af
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-01-15 21:31:56 +0800

    根据测试数据文件夹中的文件，爬取对应的在线数据进行比较

--- unittest/test_crawlers.py
+++ unittest/test_crawlers.py
@@ -1,5 +1,8 @@
 import os
+import re
 import sys
+from glob import glob
+
 import pretty_errors
 from tqdm import tqdm
 
@@ -42,11 +45,11 @@ def create_local_data(dvdid_list: list):
     return
 
 
-def compare(dvdid):
+def compare(dvdid, scraper, file):
     """从本地的数据文件生成Movie实例，并与在线抓取到的数据进行比较"""
-    path = data_dir + os.sep + f'{dvdid} (javbus).json'
-    local = Movie(from_file=path)
+    local = Movie(from_file=file)
     online = Movie(dvdid)
+    parse_data = getattr(sys.modules[f'web.{scraper}'], 'parse_data')
     parse_data(online)
     # 解包数据再进行比较，以便测试不通过时快速定位不相等的键值
     local_vars = vars(local)
@@ -55,8 +58,20 @@ def compare(dvdid):
         assert v == local_vars.get(k, None)
 
 
-def test_case1():
-    compare('IPX-177')
+def test_auto_compare():
+    """根据测试数据文件夹中的文件，爬取对应的在线数据进行比较"""
+    mods = [f'web.{name}' for name in scrapers]
+    for i in mods:
+        __import__(i)
+    data_files = glob(data_dir + os.sep + '*.json')
+    print('')   # 打印空行，避免与pytest的输出同行显示
+    for file in data_files:
+        basename = os.path.basename(file)
+        match = re.match(r"([-\w]+) \((\w+)\)", basename, re.I)
+        if match:
+            avid, scraper = match.groups()
+            print(f'Comparing {avid} with {scraper} scraper...')
+            compare(avid, scraper, file)
 
 
 if __name__ == "__main__":
