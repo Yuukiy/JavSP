c516ebec7bfd4857a41a241555ea8b5fc05d1004
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-02-07 00:06:38 +0800

    javlib: 改进对多个搜索结果的处理，尝试自动选择，无法自动选择时报错提示用户

--- web/javlib.py
+++ web/javlib.py
@@ -25,17 +25,38 @@ def parse_data(movie: MovieInfo):
         # 在有多个结果时，JavLibrary不会自动跳转（此时无法获取到标题），需要进一步处理
         video_tags = html.xpath("//div[@class='video'][@id]/a")
         # 通常第一部影片就是我们要找的，但是以免万一还是遍历所有搜索结果
+        pre_choose = []
         for tag in video_tags:
             tag_dvdid = tag.xpath("div[@class='id']/text()")[0]
             if tag_dvdid == movie.dvdid:
-                new_url = tag.xpath("@href")[0]
-                html = get_html(new_url)
-                container = html.xpath("/html/body/div/div[@id='rightcolumn']")[0]
-                title_tag = container.xpath("div/h3/a/text()")
-                logger.debug(f"'{movie.dvdid}'存在多个搜索结果，已自动选择: {new_url}")
-                break
-        else:
+                pre_choose.append(tag)
+        match_count = len(pre_choose)
+        if match_count == 0:
             logger.error(f"'{movie.dvdid}': 无法获取到影片结果")
+            return
+        elif match_count == 1:
+            new_url = pre_choose[0].get('href')
+            logger.debug(f"'{movie.dvdid}': 遇到多个搜索结果，已自动选择: {new_url}")
+        elif match_count == 2:
+            no_blueray = []
+            for tag in pre_choose:
+                if 'ブルーレイディスク' not in tag.get('title'):    # Blu-ray Disc
+                    no_blueray.append(tag)
+            no_blueray_count = len(no_blueray)
+            if no_blueray_count == 1:
+                new_url = no_blueray[0].get('href')
+                logger.debug(f"'{movie.dvdid}': 存在{match_count}个同番号搜索结果，为避免蓝光封面畸变，已自动选择: {new_url}")
+            else:
+                logger.error(f"'{movie.dvdid}': 存在{match_count}个搜索结果但是均非蓝光版，未避免误处理，已全部忽略")
+                return
+        else:
+            # 暂未发现有超过2个搜索结果的，保险起见还是进行检查
+            logger.error(f"'{movie.dvdid}': 出现{match_count}个完全匹配目标番号的搜索结果，未避免误处理，已全部忽略")
+            return
+        # 重新抓取网页
+        html = get_html(new_url)
+        container = html.xpath("/html/body/div/div[@id='rightcolumn']")[0]
+        title_tag = container.xpath("div/h3/a/text()")
     title = title_tag[0]
     cover = container.xpath("//img[@id='video_jacket_img']/@src")[0]
     info = container.xpath("//div[@id='video_info']")[0]
@@ -66,6 +87,6 @@ def parse_data(movie: MovieInfo):
 
 
 if __name__ == "__main__":
-    movie = MovieInfo('IPZ-037')
+    movie = MovieInfo('SSNI-589')
     parse_data(movie)
     print(movie)
