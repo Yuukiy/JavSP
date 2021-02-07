ca6bb6015bfdfaec8d678ed1f3ec1d8c6f013133
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-02-07 19:44:02 +0800

    config.py: 重新组织代码结构

--- core/config.py
+++ core/config.py
@@ -20,19 +20,6 @@ root_logger.addHandler(file_handler)
 logger = logging.getLogger(__name__)
 
 
-def is_url(url: str):
-    """判断给定的字符串是否是有效的带协议字段的URL"""
-    # https://stackoverflow.com/a/7160778/6415337
-    pattern = re.compile(
-        r'^(?:http)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|'     #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?'      # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return re.match(pattern, url) is not None
-
-
 class DotDict(dict):
     """Access dict value with 'dict.key' grammar"""
     __getattr__ = dict.get
@@ -60,95 +47,61 @@ class Config(configparser.ConfigParser):
             except:
                 super(Config, self).read(filenames)
 
-    def norm_config(self):
-        """对配置中必要的项目进行格式转换，以便于其他模块直接使用"""
-        # 扫描所有section和key，如果某个sec/key定义了对应的'_norm_XXX'方法，则调用该方法
-        methods = [name for name in dir(self) if callable(getattr(self, name))]
-        norm_methods = [name for name in methods if name.startswith('_norm_')]
-        for sec in self.sections():
-            sec_norm_method = '_norm_' + sec
-            if sec_norm_method in norm_methods:
-                getattr(self, sec_norm_method)()
-                continue   # 如果有sec级别的norm方法，就不再检查其下的key的norm方法
-            for key, value in self[sec].items():
-                key_norm_method = '_norm_' + key
-                if key_norm_method in norm_methods:
-                    func = getattr(self, key_norm_method)
-                    self._sections[sec][key] = func(value)
-        self.norm_boolean()
+    def validate(self):
+        """对配置中必要的项目进行验证和转换，以便于其他模块直接使用"""
+        # norm_config需要作为类的方法公开，以方便调用
+        # 由norm_config间接调用的那些实际进行转换的函数并不应当被公开，所以它们组织为模块内的函数而不是类的方法
+        norm_int(self)
+        norm_tuples(self)
+        norm_boolean(self)
+        validate_proxy(self)
+        import_crawlers(self)
+        convert_naming_rule(self)
+        # 作为配置模块，始终检查免代理地址；由各个抓取器中根据代理情况选择是否启用免代理地址
+        check_proxy_free_url(self)
 
-    def _norm_Network(self):
-        """retry: 转换为数字"""
-        self.Network.retry = self.getint('Network', 'retry')
-        self.Network.timeout = self.getint('Network', 'timeout')
 
-    def _norm_Priority(self):
-        """Priority: 按配置的抓取器顺序转换为内部的抓取器函数列表"""
-        sec = self['Priority']
-        unknown_mods = []
-        for typ, cfg_str in sec.items():
-            mods = cfg_str.split(',')
-            valid_mods = []
-            for name in mods:
-                try:
-                    # 如果fc2fan本地镜像的路径无效，则跳过它
-                    if name == 'fc2fan' and (not os.path.isdir(self.Crawler.fc2fan_local_path)):
-                        logger.debug('由于未配置有效的fc2fan路径，已跳过该抓取器')
-                        continue
-                    mod = 'web.' + name
-                    __import__(mod)
-                    valid_mods.append(mod)
-                except ModuleNotFoundError:
-                    unknown_mods.append(name)
-            self._sections['Priority'][typ] = tuple(valid_mods)
-        if unknown_mods:
-            logger.warning('  无效的抓取器: ' + ', '.join(unknown_mods))
+def is_url(url: str):
+    """判断给定的字符串是否是有效的带协议字段的URL"""
+    # https://stackoverflow.com/a/7160778/6415337
+    pattern = re.compile(
+        r'^(?:http)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|'     #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?'      # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(pattern, url) is not None
 
-    def _norm_ProxyFree(self):
-        """ProxyFree: 仅接受有效的URL"""
-        sec = self['ProxyFree']
-        for site, url in sec.items():
-            url = url.lower()
-            if not url.startswith('http'):
-                url = 'http://' + url
-            if is_url(url):
-                sec[site] = url
-            else:
-                sec[site] = ''
 
-    def _norm_NamingRule(self):
-        """NamingRule: 转换为字符串Template"""
-        combine = self.NamingRule.output_folder + os.sep + self.NamingRule.save_dir
-        path_t = Template(combine)
-        file_t = Template(self.NamingRule.filename)
-        self.NamingRule.save_dir = path_t
-        self.NamingRule.filename = file_t
+def norm_int(cfg: Config):
+    """转换所有的整数类型配置"""
+    cfg.Network.retry = cfg.getint('Network', 'retry')
+    cfg.Network.timeout = cfg.getint('Network', 'timeout')
 
-    @staticmethod
-    def _norm_media_ext(cfg_str: str) -> tuple:
-        """media_ext: 转换为全小写的.ext格式的元组"""
-        items = cfg_str.lower().split(';')
-        exts = [i if i.startswith('.') else '.'+i for i in items]
-        return tuple(set(exts))
 
-    @staticmethod
-    def _norm_ignore_folder(cfg_str: str) -> tuple:
-        """ignore_folder: 转换为元组"""
-        return tuple(cfg_str.split(';'))
+def norm_tuples(cfg: Config):
+    """将特定的配置转换为元组类型，便于迭代的同时也防止误修改"""
+    # media_ext: 转换为全小写的.ext格式的元组
+    items = cfg.File.media_ext.lower().split(';')
+    exts = [i if i.startswith('.') else '.'+i for i in items]
+    cfg.File.media_ext = tuple(exts)
+    # ignore_folder: 转换为元组
+    items = cfg.File.ignore_folder.split(';')
+    cfg.File.ignore_folder = tuple(items)
+    # required_keys: 转换为元组
+    items = cfg.Crawler.required_keys.split(',')
+    cfg.Crawler.required_keys = tuple(items)
 
-    @staticmethod
-    def _norm_required_keys(cfg_str: str) -> tuple:
-        """required_keys: 转换为元组"""
-        return tuple(cfg_str.lower().split(','))
 
-    def norm_boolean(self):
-        """转换所有的布尔类型配置"""
-        for sec, key in [
+def norm_boolean(cfg: Config):
+    """转换所有的布尔类型配置"""
+    for sec, key in [
             ('Crawler', 'hardworking_mode'),
             ('Crawler', 'remove_actor_in_title'),
             ('NFO', 'add_genre_to_tag')
         ]:
-            self._sections[sec][key] = self.getboolean(sec, key)
+        cfg._sections[sec][key] = cfg.getboolean(sec, key)
 
 
 def validate_proxy(cfg: Config):
@@ -164,17 +117,59 @@ def validate_proxy(cfg: Config):
     cfg.Network.proxy = proxies
 
 
-logger.info('读取配置...')
+def import_crawlers(cfg: Config):
+    """按配置的抓取器顺序转换为的抓取器函数列表"""
+    unknown_mods = []
+    for typ, cfg_str in cfg.Priority.items():
+        mods = cfg_str.split(',')
+        valid_mods = []
+        for name in mods:
+            try:
+                # 导入fc2fan抓取器的前提: 配置了fc2fan的本地路径
+                if name == 'fc2fan' and (not os.path.isdir(cfg.Crawler.fc2fan_local_path)):
+                    logger.debug('由于未配置有效的fc2fan路径，已跳过该抓取器')
+                    continue
+                import_name = 'web.' + name
+                __import__(import_name)
+                valid_mods.append(import_name)  # 抓取器有效: 使用完整模块路径，便于程序实际使用
+            except ModuleNotFoundError:
+                unknown_mods.append(name)       # 抓取器无效: 仅使用模块名，便于显示
+        cfg._sections['Priority'][typ] = tuple(valid_mods)
+    if unknown_mods:
+        # 如果直接运行config.py，抓取器会导入失败，但是不影响config被作为模块导入时
+        if __name__ != "__main__":
+            logger.warning('配置的抓取器无效: ' + ', '.join(unknown_mods))
+        else:
+            print("直接运行'config.py'时无法导入抓取器，导入失败: " + ', '.join(unknown_mods))
+
+
+def convert_naming_rule(cfg: Config):
+    """NamingRule: 转换为字符串Template"""
+    combine = cfg.NamingRule.output_folder + os.sep + cfg.NamingRule.save_dir
+    path_t = Template(combine)
+    file_t = Template(cfg.NamingRule.filename)
+    cfg.NamingRule.save_dir = path_t
+    cfg.NamingRule.filename = file_t
+
+
+def check_proxy_free_url(cfg: Config):
+    """检查免代理URL的格式是否有效"""
+    sec = cfg['ProxyFree']
+    for site, url in sec.items():
+        url = url.lower()
+        if not url.startswith('http'):
+            url = 'http://' + url
+        sec[site] = url if is_url(url) else ''
+
 
 cfg = Config()
+logger.info('读取配置...')
 cfg_file = os.path.join(os.path.dirname(__file__), 'config.ini')
 cfg.read(cfg_file)
-# cfg.norm_config()
-validate_proxy(cfg)
+cfg.validate()
 
 
 if __name__ == "__main__":
-    import os
     import pretty_errors
     pretty_errors.configure(display_link=True)
 
