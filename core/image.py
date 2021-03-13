3abfe1740b055f7ccfae975f3196f3dff9bb1c99
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-03-13 14:09:56 +0800

    调整poster的裁剪比例为1.42以保持poster画面内容的完整

--- core/image.py
+++ core/image.py
@@ -7,12 +7,12 @@ __all__ = ['crop_poster', 'get_pic_size']
 
 def crop_poster(fanart_file, poster_file):
     """将给定的fanart图片文件裁剪为适合poster尺寸的图片"""
-    # 本项目根据KODI的文档来裁剪poster。对于其他的媒体服务器（如Jellyfin），
-    # 由于未在其文档中找到对此的说明，因此假定它们的相关spec均符合先行者KODI的规范:
-    # https://kodi.wiki/view/Artwork_types#poster
+    # Kodi的宽高比为2:3，但是按照这个比例来裁剪会导致poster画面不完整，
+    # 因此按照poster画面比例来裁剪，这样的话虽然在显示时可能有轻微变形，但是画面是完整的
     fanart = Image.open(fanart_file)
     fanart_w, fanart_h = fanart.size
-    poster_w = int(fanart_h / 3 * 2)
+    # 1.42 = 2535/1785（高清封面）, 539/379（普通封面）
+    poster_w = int(fanart_h / 1.42)
     box = (fanart_w-poster_w, 0, fanart_w, fanart_h)
     poster = fanart.crop(box)
     # quality: from doc, default is 75, values above 95 should be avoided
