416fce646c74287152b3e44d22fc16c07056af79
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-04-18 17:43:09 +0800

    添加百度翻译的函数实现

--- web/translate.py
+++ web/translate.py
@@ -2,7 +2,9 @@
 import os
 import sys
 import uuid
+import random
 import requests
+from hashlib import md5
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -27,5 +29,20 @@ def bing_translate(texts, to='zh-Hans'):
     return response[0]['translations']
 
 
+def baidu_translate(texts, to='zh'):
+    """使用百度翻译文本（默认翻译为简体中文）"""
+    api_url = "https://fanyi-api.baidu.com/api/trans/vip/translate"
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    appid = cfg.Translate.baidu_appid
+    appkey = cfg.Translate.baidu_key
+    salt = random.randint(0, 0x7FFFFFFF)
+    sign_input = appid + texts + str(salt) + appkey
+    sign = md5(sign_input.encode('utf-8')).hexdigest()
+    payload = {'appid': appid, 'q': texts, 'from': 'auto', 'to': to, 'salt': salt, 'sign': sign}
+    r = requests.post(api_url, params=payload, headers=headers)
+    result = r.json()
+    return result
+
+
 if __name__ == "__main__":
-    print(bing_translate('Hello world~'))
+    print(baidu_translate('Hello world~'))
