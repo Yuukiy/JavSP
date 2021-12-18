5d66d48d7270aee3c0fe70a994eb48fdd85ec543
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-12-18 23:56:29 +0800

    根据识别出的人脸区域的大小对不同角度的结果进行筛选，并增加便于区分识别区域信息的功能

--- core/opencv.py
+++ core/opencv.py
@@ -33,23 +33,41 @@ def rotate_bound(image, angle):
     return cv2.warpAffine(image, M, (nW, nH))
 
 
+def isValidArea(area):
+    """通过识别出的人脸区域大小判断识别结果是否有效"""
+    (x, y, w, h) = area
+    # 目前的参数是根据840x450左右大小的图片设置的
+    if (100 <= w <= 200) and (100 <= h <= 200):
+        return True
+    else:
+        return False
+
+
 def detect_faces(img_path, debug=False):
     img = cv2.imread(img_path)
     faces = []
     # OpenCV的检测效果似乎受图像角度影响很大，因此要尝试旋转图片
-    for angle in range(-65, 66, 10):
+    rotate_angles = (0, 10, -10, 20, -20, 30, -30, 40, -40, 50, -50, 60, -60)
+    for angle in rotate_angles:
         rotated = rotate_bound(img, angle)
         grey = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(grey, 1.1, 4)
+        for area in faces:
+            if isValidArea(area) and (not debug):
+                return area
+            # 有效区域用绿框标记，无效区域用红框标记
+            color = (0, 255, 0) if isValidArea(area) else (0, 0, 255)
+            (x, y, w, h) = area
+            cv2.rectangle(rotated, (x, y), (x+w, y+h), color, 2)
+            recog_info = f'({x},{y}), {w}x{h}'
+            (h2, w2) = rotated.shape[:2]
+            pos = (min(w2-170, x), max(y-10, 12))    # 避免信息显示到绘图区外
+            cv2.putText(rotated, recog_info, pos, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
         if debug:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(rotated, (x, y), (x+w, y+h), (0, 255, 0), 2)
             win_name = "Actress detection"
             cv2.imshow(win_name, rotated)
             cv2.setWindowTitle(win_name, win_name + f" with ratotion {angle} degrees")
             cv2.waitKey(0)
-        # 过滤掉大概率不符合条件的结果
-        pass
     return faces
 
 
@@ -59,6 +77,6 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True,
         help="path to the image file")
-    args = vars(ap.parse_args(['-i', 'siro-4701.jpg']))
+    args = vars(ap.parse_args(['-i', 'YPPx.jpg']))
 
     detect_faces(args['image'], True)
