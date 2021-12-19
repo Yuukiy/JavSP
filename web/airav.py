c54d335c1ae43ec0318f35f453214f1d5234470c
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2021-12-19 21:14:44 +0800

    增加airav的timeout时长阈值到20秒，以应对服务器不稳定的情况

--- web/airav.py
+++ web/airav.py
@@ -13,6 +13,8 @@ from core.datatype import MovieInfo
 # 初始化Request实例
 request = Request()
 request.headers['Accept-Language'] = 'zh-TW,zh;q=0.9'
+# 近期airav服务器似乎不稳定，时好时坏，单次查询平均在17秒左右，timeout时间增加到20秒
+request.timeout = 20
 
 
 logger = logging.getLogger(__name__)
