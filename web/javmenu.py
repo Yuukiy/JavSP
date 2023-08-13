dfa221b1b4d9a8e9a1ea9877bc62bd5b9c5a22ed
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2023-08-13 21:29:09 +0800

    javmenu: 适配站点变化: 解决标题带广告和获取不到磁链的问题

--- web/javmenu.py
+++ web/javmenu.py
@@ -28,10 +28,11 @@ def parse_data(movie: MovieInfo):
         raise MovieNotFoundError(__name__, movie.dvdid)
 
     html = resp2html(r)
-    container = html.xpath("//div[@class='col-md-8 px-0']")[0]
+    container = html.xpath("//div[@class='col-md-9 px-0']")[0]
     title = container.xpath("div[@class='col-12 mb-3']/h1/strong/text()")[0]
     # 竟然还在标题里插广告，真的疯了。要不是我已经写了抓取器，才懒得维护这个破站
     title = title.replace('  | JAV目錄大全 | 每日更新', '')
+    title = title.replace('  免費在線看', '')
     cover_tag = container.xpath("//div[@class='single-video']")[0]
     video_tag = cover_tag.find('video')
     if video_tag is not None:
@@ -56,9 +57,9 @@ def parse_data(movie: MovieInfo):
         pre_id = items[-3] + '/' + items[-1]
         genre.append(tag.text.strip())
         genre_id.append(pre_id)
-        # genra的链接中含有censored字段，但是无法用来判断影片是否有码，因为完全不可靠……
+        # genre的链接中含有censored字段，但是无法用来判断影片是否有码，因为完全不可靠……
     actress = info.xpath("div/span[contains(text(), '女優:')]/following-sibling::*/a/text()") or None
-    magnet_table = container.xpath("//div[@id='download-tab']//tbody")
+    magnet_table = container.xpath("//table[contains(@class, 'magnet-table')]/tbody")
     if magnet_table:
         magnet_links = magnet_table[0].xpath("tr/td/a/@href")
         # 它的FC2数据是从JavDB抓的，JavDB更换图片服务器后它也跟上了，似乎数据更新频率还可以
