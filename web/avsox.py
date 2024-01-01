bdd11086b2340466731591097ef1770bd6d1ef46
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2024-01-01 22:51:07 +0800

    avsox: 支持解析FC2影片

--- web/avsox.py
+++ web/avsox.py
@@ -17,12 +17,15 @@ base_url = cfg.ProxyFree.avsox
 def parse_data(movie: MovieInfo):
     """解析指定番号的影片数据"""
     # avsox无法直接跳转到影片的网页，因此先搜索再从搜索结果中寻找目标网页
-    html = get_html(f'{base_url}/cn/search/{movie.dvdid}')
+    full_id = movie.dvdid
+    if full_id.startswith('FC2-'):
+        full_id = full_id.replace('FC2-', 'FC2-PPV-')
+    html = get_html(f'{base_url}/cn/search/{full_id}')
     ids = html.xpath("//div[@class='photo-info']/span/date[1]/text()")
     urls = html.xpath("//a[contains(@class, 'movie-box')]/@href")
     ids_lower = list(map(str.lower, ids))
-    if movie.dvdid.lower() in ids_lower:
-        url = urls[ids_lower.index(movie.dvdid.lower())]
+    if full_id.lower() in ids_lower:
+        url = urls[ids_lower.index(full_id.lower())]
     else:
         raise MovieNotFoundError(__name__, movie.dvdid, ids)
 
@@ -35,16 +38,17 @@ def parse_data(movie: MovieInfo):
     dvdid = info.xpath("p/span[@style]/text()")[0]
     publish_date = info.xpath("p/span[text()='发行时间:']")[0].tail.strip()
     duration = info.xpath("p/span[text()='长度:']")[0].tail.replace('分钟', '').strip()
+    producer, serial = None, None
     producer_tag = info.xpath("p[text()='制作商: ']")[0].getnext().xpath("a")
     if producer_tag:
-        movie.producer = producer_tag[0].text_content()
+        producer = producer_tag[0].text_content()
     serial_tag = info.xpath("p[text()='系列:']")
     if serial_tag:
-        movie.serial = serial_tag[0].getnext().xpath("a/text()")[0]
+        serial = serial_tag[0].getnext().xpath("a/text()")[0]
     genre = info.xpath("p/span[@class='genre']/a/text()")
     actress = container.xpath("//a[@class='avatar-box']/span/text()")
 
-    movie.dvdid = dvdid
+    movie.dvdid = dvdid.replace('FC2-PPV-', 'FC2-')
     movie.url = url
     movie.title = title.replace(dvdid, '').strip()
     movie.cover = cover
@@ -52,6 +56,12 @@ def parse_data(movie: MovieInfo):
     movie.duration = duration
     movie.genre = genre
     movie.actress = actress
+    if full_id.startswith('FC2-'):
+        # avsox把FC2作品的拍摄者归类到'系列'而制作商固定为'FC2-PPV'，这既不合理也与其他的站点不兼容，因此进行调整
+        movie.producer = serial
+    else:
+        movie.producer = producer
+        movie.serial = serial
 
 
 if __name__ == "__main__":
@@ -59,7 +69,7 @@ if __name__ == "__main__":
     pretty_errors.configure(display_link=True)
     logger.root.handlers[1].level = logging.DEBUG
 
-    movie = MovieInfo('130614-KEIKO')
+    movie = MovieInfo('FC2-718323')
     try:
         parse_data(movie)
         print(movie)
