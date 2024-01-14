3f5b726a2601ef21f35cb166575a97e0cc8b2d1a
Yuukiy <76897913+Yuukiy@users.noreply.github.com>
2024-01-14 15:35:04 +0800

    fanza: 支持解析 成人映画、同人作品、单品租赁 的数据 (Fix #192)

--- web/fanza.py
+++ web/fanza.py
@@ -4,6 +4,7 @@ import re
 import sys
 import json
 import logging
+from typing import Dict, List, Tuple
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -21,40 +22,88 @@ request.cookies = {'age_check_done': '1'}
 request.headers['Accept-Language'] = 'ja,en-US;q=0.9'
 
 
-def parse_data(movie: MovieInfo):
-    """解析指定番号的影片数据"""
-    try_urls = {
-        'videoa': f'{base_url}/digital/videoa/-/detail/=/cid={movie.cid}/', # AV
-        'anime': f'{base_url}/mono/anime/-/detail/=/cid={movie.cid}/',      # 里番
-    }
-    for type_, url in try_urls.items():
-        r = request.get(url, delay_raise=True)
-        # 404错误表明没有这部影片的数据
-        if r.status_code == 404:
-            continue
+_PRODUCT_PRIORITY = {'digital': 10, 'mono': 5, 'monthly': 2, 'rental': 1}
+_TYPE_PRIORITY = {'videoa': 10, 'anime': 8, 'nikkatsu': 6, 'doujin': 4, 'dvd': 3, 'ppr': 2, 'paradisetv': 1}
+def sort_search_result(result: List[Dict]):
+    """排序搜索结果"""
+    scores = {i['url']:(_PRODUCT_PRIORITY.get(i['product'], 0), _TYPE_PRIORITY.get(i['type'], 0)) for i in result}
+    sorted_result = sorted(result, key=lambda x:scores[x['url']], reverse=True)
+    return sorted_result
 
-        r.raise_for_status()
-        html = resp2html(r)
-        break
-    else:
-        raise MovieNotFoundError(__name__, movie.cid)
+
+def get_urls_of_cid(cid: str) -> Tuple[str, str]:
+    """搜索cid可能的影片URL"""
+    r = request.get(f"https://www.dmm.co.jp/search/?redirect=1&enc=UTF-8&category=&searchstr={cid}&commit.x=0&commit.y=0")
+    if r.status_code == 404:
+        raise MovieNotFoundError(__name__, cid)
+    r.raise_for_status()
+    html = resp2html(r)
+    result = html.xpath("//ul[@id='list']/li/div/p/a/@href")
+    parsed_result = {}
+    for url in result:
+        items = url.split('/')
+        type_, cid = None, None
+        for i, part in enumerate(items):
+            if part == '-':
+                product, type_ = items[i-2], items[i-1]
+            elif part.startswith('cid='):
+                cid = part[4:]
+                new_url = '/'.join(i for i in items if not i.startswith('?')) + '/'
+                parsed_result.setdefault(cid, []).append({'product': product, 'type': type_, 'url': new_url})
+                break
+    if cid not in parsed_result:
+        if len(result) > 0:
+            logger.debug(f"Unknown URL in search result: " + ', '.join(result))
+        raise MovieNotFoundError(__name__, cid)
+    sorted_result = sort_search_result(parsed_result[cid])
+    return sorted_result
+
+
+def resp2html_wrapper(resp):
+    html = resp2html(resp)
     if 'not available in your region' in html.text_content():
         raise SiteBlocked('FANZA不允许从当前IP所在地区访问，请检查你的网络和代理服务器设置')
+    return html
 
-    movie.url = url
-    parse_func = globals()[f'parse_{type_}_page']
-    parse_func(movie, html)
+
+def parse_data(movie: MovieInfo):
+    """解析指定番号的影片数据"""
+    default_url = f'{base_url}/digital/videoa/-/detail/=/cid={movie.cid}/'
+    r0 = request.get(default_url, delay_raise=True)
+    if r0.status_code == 404:
+        urls = get_urls_of_cid(movie.cid)
+        for d in urls:
+            func_name = f"parse_{d['type']}_page"
+            if func_name in globals():
+                parse_func = globals()[func_name]
+            else:
+                logger.debug(f"不知道怎么解析 fanza {d['type']} 的页面: {d['url']}")
+                continue
+            r = request.get(d['url'])
+            html = resp2html_wrapper(r)
+            try:
+                parse_func(movie, html)
+                movie.url = d['url']
+                break
+            except:
+                if id(d) == id(urls[-1]):
+                    raise
+    else:
+        html = resp2html_wrapper(r0)
+        parse_videoa_page(movie, html)
+        movie.url = default_url
 
 
 def parse_videoa_page(movie: MovieInfo, html):
     """解析AV影片的页面布局"""
-    title = html.xpath("//h1[@id='title']/text()")[0]
+    title = html.xpath("//div[@class='hreview']/h1/text()")[0]
     # 注意: 浏览器在渲染时会自动加上了'tbody'字段，但是原始html网页中并没有，因此xpath解析时还是要按原始网页的来
     container = html.xpath("//table[@class='mg-b12']/tr/td")[0]
     cover = container.xpath("//div[@id='sample-video']/a/@href")[0]
     # 采用'配信開始日'作为发布日期: https://www.zhihu.com/question/57513172/answer/153219083
-    date_str = container.xpath("//td[text()='配信開始日：']/following-sibling::td/text()")[0].strip()
-    publish_date = date_str.replace('/', '-')
+    date_tag = container.xpath("//td[text()='配信開始日：']/following-sibling::td/text()")
+    if date_tag:
+        movie.publish_date = date_tag[0].strip().replace('/', '-')
     duration_str = container.xpath("//td[text()='収録時間：']/following-sibling::td/text()")[0].strip()
     match = re.search(r'\d+', duration_str)
     if match:
@@ -75,19 +124,23 @@ def parse_videoa_page(movie: MovieInfo, html):
     # if label_tag:
     #     label = label_tag[0].strip()
     # fanza会把促销信息也写进genre……因此要根据tag指向的链接类型进行筛选
-    genre_tags = container.xpath("//td[text()='ジャンル：']/following-sibling::td/a[contains(@href,'?keyword=')]")
+    genre_tags = container.xpath("//td[text()='ジャンル：']/following-sibling::td/a[contains(@href,'?keyword=') or contains(@href,'article=keyword')]")
     genre, genre_id = [], []
     for tag in genre_tags:
         genre.append(tag.text.strip())
         genre_id.append(tag.get('href').split('=')[-1].strip('/'))
     cid = container.xpath("//td[text()='品番：']/following-sibling::td/text()")[0].strip()
-    plot = container.xpath("//div[@class='mg-b20 lh4']/text()")[0].strip()
+    plot = container.xpath("//div[contains(@class, 'mg-b20 lh4')]/text()")[0].strip()
     preview_pics = container.xpath("//a[@name='sample-image']/img/@src")
-    score_str = container.xpath("//p[@class='d-review__average']/strong/text()")[0].strip()
-    match = re.search(r'\d+', score_str)
-    if match:
-        score = float(match.group()) * 2
-        movie.score = f'{score:.2f}'
+    score_tag = container.xpath("//p[@class='d-review__average']/strong/text()")
+    if score_tag:
+        match = re.search(r'\d+', score_tag[0].strip())
+        if match:
+            score = float(match.group()) * 2
+            movie.score = f'{score:.2f}'
+    else:
+        score_img = container.xpath("//td[@class='dcd-review__anchor_content']/img/@src")[0]
+        movie.score = int(score_img.split('/')[-1].split('.')[0]) # 00, 05 ... 50
     
     if cfg.Crawler.hardworking_mode:
         # 预览视频是动态加载的，不在静态网页中
@@ -109,7 +162,6 @@ def parse_videoa_page(movie: MovieInfo, html):
     movie.cid = cid
     movie.title = title
     movie.cover = cover
-    movie.publish_date = publish_date
     movie.actress = actress
     movie.genre = genre
     movie.genre_id = genre_id
@@ -125,8 +177,9 @@ def parse_anime_page(movie: MovieInfo, html):
     cover = container.xpath("//a[@name='package-image']/@href")[0]
     date_str = container.xpath("//td[text()='発売日：']/following-sibling::td/text()")[0].strip()
     publish_date = date_str.replace('/', '-')
-    duration_str = container.xpath("//td[text()='収録時間：']/following-sibling::td/text()")[0].strip()
-    duration = duration_str.replace('分', '')
+    duration_tag = container.xpath("//td[text()='収録時間：']/following-sibling::td/text()")
+    if duration_tag:
+        movie.duration = duration_tag[0].strip().replace('分', '')
     serial_tag = container.xpath("//td[text()='シリーズ：']/following-sibling::td/a/text()")
     if serial_tag:
         movie.serial = serial_tag[0].strip()
@@ -148,7 +201,6 @@ def parse_anime_page(movie: MovieInfo, html):
     movie.title = title
     movie.cover = cover
     movie.publish_date = publish_date
-    movie.duration = duration
     movie.genre = genre
     movie.genre_id = genre_id
     movie.plot = plot
@@ -157,12 +209,18 @@ def parse_anime_page(movie: MovieInfo, html):
     movie.uncensored = False    # 服务器在日本且面向日本国内公开发售，不会包含无码片
 
 
+# parse_dvd_page = parse_videoa_page    # 118wtktabf067
+parse_ppr_page = parse_videoa_page
+parse_nikkatsu_page = parse_videoa_page
+parse_doujin_page = parse_anime_page
+
+
 if __name__ == "__main__":
     import pretty_errors
     pretty_errors.configure(display_link=True)
     logger.root.handlers[1].level = logging.DEBUG
 
-    movie = MovieInfo(cid='62knbm009')
+    movie = MovieInfo(cid='1stars931r')
     try:
         parse_data(movie)
         print(movie)
