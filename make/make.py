a67f1e6434a7490d077fa22d74011bdfbe032290
glyh <lyhokia@gmail.com>
2024-04-16 22:42:28 +0800

    merge ver hook and make.py

--- make/make.py
+++ make/make.py
@@ -1,24 +1,51 @@
 #!/usr/bin/env python
 
-import subprocess
 import os
 from pathlib import Path
 import PyInstaller.__main__
 import shutil
+import subprocess
 
-from . import gen_ver_hook
+def get_version():
+    auto_ver = ""
+    run = subprocess.run(['git', 'describe', '--tags', '--long'], capture_output=True, encoding='utf-8')
+    if run.returncode == 0:
+        desc = run.stdout.strip()
+        tag_name, minor, _ = desc.split('-')
+        if int(minor) == 0: # means current commit exactly matches the tag
+            auto_ver = tag_name
+        else:
+            if tag_name.count('.') == 1:
+                auto_ver = tag_name + '.0.' + minor
+            else:
+                auto_ver = tag_name + '.' + minor
+    else:
+        auto_ver = "v0.unknown"
+    return auto_ver
+
+def gen_ver_hook(version, hook_file):
+    with open(hook_file, 'wt', encoding='utf-8') as f:
+        f.write('import sys\n')
+        f.write("setattr(sys, 'javsp_version', '" + version + "')\n")
 
 def main():
+    version = get_version()
+    print(f"Packaging JavSP {version}...")
 
     script_path = Path(os.path.dirname(os.path.realpath(__file__)))
     os.chdir(script_path)
 
-    gen_ver_hook.gen_ver_hook()
+    gen_ver_hook(version, 'ver_hook.py')
 
     PyInstaller.__main__.run([
         '--clean',
         './build.spec'
     ])
+
+    # create zip
+    sys_info = os.uname()
+    zip_file = f'dist/JavSP-{version}-{sys_info.sysname}-{sys_info.machine}.zip'
+    shutil.make_archive(zip_file, 'zip', 'dist')
 
     os.remove('ver_hook.py')
 
