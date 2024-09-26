d964c98e37d8ac7cd93ba3d3d6db3868e34298be
glyh <lyhokia@gmail.com>
2024-09-26 16:50:33 +0800

    allow arzon & arzon_iv to be used as crawlers

--- javsp/core/config.py
+++ javsp/core/config.py
@@ -34,6 +34,8 @@ class CrawlerID(str, Enum):
     mgstage = 'mgstage'
     njav = 'njav'
     prestige = 'prestige'
+    arzon = 'arzon'
+    arzon_iv = 'arzon_iv'
 
 class Network(BaseConfig):
     proxy_server: Url | None
