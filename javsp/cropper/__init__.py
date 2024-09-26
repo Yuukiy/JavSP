df4c8d468ff7527368ef0016c82dd356b9b046a0
glyh <lyhokia@gmail.com>
2024-09-27 03:07:25 +0800

    remove the need for numpy

--- javsp/cropper/__init__.py
+++ javsp/cropper/__init__.py
@@ -3,7 +3,7 @@ from javsp.cropper.interface import Cropper, DefaultCropper
 from javsp.cropper.yuface_crop import YufaceCropper
 
 def get_cropper(engine: YufaceEngine | None) -> Cropper:
+    if engine is None:
+        return DefaultCropper()
     if engine.name == 'yuface':
         return YufaceCropper()
-    return DefaultCropper()
-
--- javsp/cropper/yuface_crop.py
+++ javsp/cropper/yuface_crop.py
@@ -1,34 +1,17 @@
-from typing import List
 from PIL import Image
 from javsp.cropper.interface import Cropper, DefaultCropper
 from javsp.cropper.utils import get_bound_box_by_face
-from yuface import detect
-import numpy as np
-
-def pillow2bgr(im: Image.Image) -> List[List[List[int]]]:
-    bytes = list(im.convert('RGB').tobytes())
-    output: List[List[List[int]]] = []
-    ptr = 0
-    for col in range(im.width):
-        output.append([])
-        for _ in range(im.height):
-            r, g, b = bytes[ptr:ptr+3]
-            output[col].append([b, g, r])
-            ptr += 3
-    return output
+from yuface import detectRGB
 
 class YufaceCropper(Cropper):
     def crop_specific(self, fanart: Image.Image, ratio: float) -> Image.Image:
         try: 
-            image_mat = np.array(fanart.convert('RGB'))[:, :, ::-1]
-            confs, bboxes, _ = detect(image_mat)
-            conf_bboxes = list(zip(confs, bboxes))
-            conf_bboxes.sort(key=lambda conf_bbox: -conf_bbox[0])
-            face = conf_bboxes[0][1]
+            bbox_confs = detectRGB(fanart.width, fanart.height, fanart.convert('RGB').tobytes())
+            bbox_confs.sort(key=lambda conf_bbox: -conf_bbox[4]) # last arg stores confidence
+            face = bbox_confs[0][:-1]
             poster_box = get_bound_box_by_face(face, fanart.size, ratio)
             return fanart.crop(poster_box)
         except Exception as e:
-            print("fuck: ", e)
             return DefaultCropper().crop_specific(fanart, ratio)
 
 if __name__ == '__main__':
